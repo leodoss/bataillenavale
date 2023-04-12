c3c06339c20a7f257d0b9155a2863e1da387535d
Leo <62615866+leodoss@users.noreply.github.com>
2023-04-12 12:25:29 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,21 +1,28 @@
 from tkinter import *
 
 class Grille:
-    def __init__(self, taille):
+    def __init__(self, x, y, taille):
         self.taille = taille
         self.grille = []
         self.bateausurgrille = 0
         for i in range(taille):
             self.grille.append([])
             for j in range(taille):
-                case = Canvas(fenetre,width=25,height=25,bg='#1E90FF')
-                case.grid(column=i,row=j)
-                case.bind("<Button-1>", lambda e,a = i, b =j: self.tir(a, b))
+                case = Canvas(fenetre, width=25, height=25, bg="#1E90FF", highlightbackground="white")
+                case.grid(column=i+x,row=j+y)
                 self.grille[i].append({"case": case, "numero": 0})
-            
+    
+    def peut_tirer(self):
+        for i in range(self.taille):
+            for j in range(self.taille):
+                self.grille[i][j]['case'].bind("<Button-1>", lambda e,a = i, b =j: self.tir(a, b))
+                
+    def peut_pas_tirer(self):
+        for i in range(self.taille):
+            for j in range(self.taille):
+                self.grille[i][j]['case'].unbind("<Button-1>")
+                                                 
     def tir(self, x, y):
-        if x < 0 or y < 0 or x >= self.taille or y >= self.taille:
-            return
         self.grille[x][y]["case"]["bg"] = "red"
     
     def est_vide(self):
@@ -23,9 +30,16 @@ class Grille:
     
     def est_remplie(self):
         return self.bateausurgrille == 5
-        
-    def placement(self, x, y, bateau):
-        pass
+    
+    def peut_placer(self):
+        for i in range(self.taille):
+            for j in range(self.taille):
+                self.grille[i][j]['case'].bind("<Button-2>", lambda e,a = i, b =j: self.placement(a, b))
+                
+    def peut_pas_placer(self):
+        for i in range(self.taille):
+            for j in range(self.taille):
+                self.grille[i][j]['case'].unbind("<Button-2>")
         
     
 class Bateau:
@@ -40,11 +54,28 @@ class Jeu:
         self.grille1 = Grille1
         self.grille2 = Grille2
         
+    def placement(self, Grille):
+        i = 0
+        while not Grille.est_rempli():
+            bateau_courant = self.bateau[i]
+            
+    
+    def placement_aux(self):
+        pass
+            
+            
+        
     def start(self):
         pass
         
 fenetre = Tk()
 fenetre.geometry("1080x700")
-g = Grille(10)
+fenetre.configure(background='white')
+
+g1 = Grille(0, 0, 10)
+g1.peut_tirer()
+separateur = Canvas(fenetre, width=25, height=25, bg="white", highlightbackground="white")
+separateur.grid(column=0, row=11)
+g2 = Grille(0, 12, 10)
 
 fenetre.mainloop()
